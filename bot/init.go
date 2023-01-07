a65159c333295c51977b79f273af7c8aa1348f1a
lizeyou0203 <36835454+lizeyou0203@users.noreply.github.com>
2023-01-07 22:28:37 +0800

    Update init.go

--- bot/init.go
+++ bot/init.go
@@ -24,5 +24,7 @@ func BotInit() {
 	if Cfg.Bot.WgpSecBot.Enabled {
 		register.RegisterBot(&WgpSecBot{})
 	}
-
+        if Cfg.Bot.MessagePusher.Enabled {
+                register.RegisterBot(&MessagePusher{})
+        }
 }
